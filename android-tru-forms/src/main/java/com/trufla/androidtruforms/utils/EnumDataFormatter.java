5f875352499fd39b57c84bd5f0c12b9d1f98a4ed
Omar El Hefny <omar.m@trufla.com>
2018-09-28 00:35:21 +0200

    feat:handle the  type when it comes as object that contains data array not array directly

--- android-tru-forms/src/main/java/com/trufla/androidtruforms/utils/EnumDataFormatter.java
+++ android-tru-forms/src/main/java/com/trufla/androidtruforms/utils/EnumDataFormatter.java
@@ -1,22 +1,48 @@
 package com.trufla.androidtruforms.utils;
 
+import android.util.Log;
 import android.util.Pair;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class EnumDataFormatter {
     public static ArrayList<Pair<Object, String>> getPairList(String string, String selector, ArrayList<String> names) {
         ArrayList<Pair<Object, String>> list = new ArrayList<>();
-        JsonArray jsonArray = new JsonParser().parse(string).getAsJsonArray();
-        for (int i = 0; i < jsonArray.size(); i++) {
-            list.add(getPairFromObject(jsonArray.get(i).getAsJsonObject(), selector, names));
+        JsonElement jsonElement;
+        JsonArray jsonArray;
+        try {
+            jsonElement = new JsonParser().parse(string);
+            jsonArray = jsonElement.isJsonArray() ? jsonElement.getAsJsonArray() : null;
+            if (jsonArray == null && jsonElement.isJsonObject() && jsonElement.getAsJsonObject().has("data")) {
+                jsonArray = jsonElement.getAsJsonObject().get("data").getAsJsonArray();
+            } else {
+                jsonArray = findArrayField(jsonElement);
+            }
+            for (JsonElement element : jsonArray) {
+                list.add(getPairFromObject(element.getAsJsonObject(), selector, names));
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
         return list;
+
+    }
+
+    private static JsonArray findArrayField(JsonElement jsonElement) {
+        JsonObject jsonObject = jsonElement.getAsJsonObject();
+        for (Map.Entry<String, JsonElement> stringJsonElementEntry : jsonObject.entrySet()) {
+            if (stringJsonElementEntry.getValue().isJsonArray())
+                return stringJsonElementEntry.getValue().getAsJsonArray();
+        }
+        return null;
     }
 
     private static Pair<Object, String> getPairFromObject(JsonObject asJsonObject, String selector, ArrayList<String> names) {
